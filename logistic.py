eb8f16ec5844b92501976dbafa757ab65766f69a
ShenDezhou <bangtech@sina.com>
2019-09-06 11:31:03 +0800

    fix some typo bugs.

--- logistic.py
+++ logistic.py
@@ -15,6 +15,8 @@ from sklearn.model_selection import train_test_split
 import pkuseg
 import datetime
 import pickle
+from sklearn_porter import Porter
+
 
 seg = pkuseg.pkuseg(model_name='web')
 train_percent=0.8
@@ -27,21 +29,22 @@ for filename in os.listdir(u"./cases"):
     data_labels = []
     log_model = LogisticRegression()
     if filename.endswith(".txt"):
+        token = filename.replace(".txt","")
         with codecs.open("./cases/"+filename, 'r', encoding='utf-8') as f:
-            text = f.read()
-            xy = text.split('|')
-            data.append(xy[0])
-            try:
-                int(xy[1])
-            except ValueError:
-                print("1:",xy[0])
-                return
-            data_labels.append(xy[1])
+            for text in f:
+                xy = text.split('|')
+                data.append(xy[0])
+    #             try:
+    #                 t=int(xy[1])
+    #             except ValueError:
+    #                 print("1:",xy[0])
+    #                 break
+                data_labels.append(xy[1])
         
         print(len(data), len(data_labels))
-        if len(data)%100!=0:
-            print("2:",filename)
-            return
+#         if len(data)%100!=0:
+#             print("2:",token)
+#             break
         
         
         vectorizer = CountVectorizer(
@@ -52,33 +55,41 @@ for filename in os.listdir(u"./cases"):
             data
         )
         features_nd = features.toarray()
-        numpy.savez_compressed("./numpy/"+filename+".npz", nd=features_nd)
-        print("3:"+filename+".npz")
+        numpy.savez_compressed("./numpy/"+token+".npz", nd=features_nd)
+        print("3:"+token+".npz")
         
         X_train, X_test, y_train, y_test  = train_test_split(
         features_nd, 
         data_labels,
         train_size=train_percent, 
         random_state=STATE)
-        print("4:split"+train_percent)
+        print("4:split", train_percent)
         
         before_training = datetime.datetime.now()
         log_model = log_model.fit(X=X_train, y=y_train)
         after_training = datetime.datetime.now()
         
-        s = pickle.dumps(log_model)
-        print("model:",s)
+        with open("./model/"+token+".pkl", "wb") as f:
+            s = pickle.dumps(log_model)
+            f.write(s)
+            print("5-1 pickle:", len(s))
+        
+#         with open("./model/"+token+".java", "w") as f:
+#             porter = Porter(log_model, language='java')
+#             s = porter.export(embed_data=True)
+#             f.write(s)
+#             print("5-2 java:", len(s))
         
         train_pred = log_model.predict(X_train)
-        print(filename,'@train-score', accuracy_score(y_train, train_pred))
-        print("5:training time(sec):"+str((after_training-before_training).total_seconds()))
+        print(token,'@train-score', accuracy_score(y_train, train_pred))
+        print("5:training time(sec):", str((after_training-before_training).total_seconds()))
         
         y_pred = log_model.predict(X_test)
-        print(filename,'@test-score', accuracy_score(y_test, y_pred))
+        print(token,'@test-score', accuracy_score(y_test, y_pred))
         print("6:test")
         continue
     else:
         continue
 
 after_model = datetime.datetime.now()
-print("ALL-DONE"+str((after_model-before_model).total_seconds()))
+print("ALL-DONE", str((after_model-before_model).total_seconds()))
